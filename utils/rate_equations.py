c04b85e5049bf9c3a3874ab44d6b84bf1b5248a6
bnaylor51 <56124987+bnaylor51@users.noreply.github.com>
2020-11-18 09:51:49 -0700

    added r2 calculation

--- utils/rate_equations.py
+++ utils/rate_equations.py
@@ -11,3 +11,10 @@ import numpy as np
 
 def simple(t, k, a, p_adj):
     return a - a * (np.exp(-(k + p_adj) * t))
+
+def calculate_r2 (actual, predicted):
+    residuals = actual - predicted
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((actual-np.mean(actual))**2)
+    r_squared = 1- ss_res/ss_tot
+    return r_squared
