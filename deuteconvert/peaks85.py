d861e908dbeb69105f012211076a733d8ff98568
bnaylor51 <56124987+bnaylor51@users.noreply.github.com>
2021-07-19 08:19:02 -0600

    Add files via upload

--- deuteconvert/peaks85.py
+++ deuteconvert/peaks85.py
@@ -1,3 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2016-2020 Bradley Naylor, Michael Porter, Kyle Cutler, Chad Quilling, J.C. Price, and Brigham Young University
+All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are permitted provided
+that the following conditions are met:
+    * Redistributions of source code must retain the
+      above copyright notice, this list of conditions
+      and the following disclaimer.
+    * Redistributions in binary form must reproduce
+      the above copyright notice, this list of conditions
+      and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the author nor the names of any contributors
+      may be used to endorse or promote products derived
+      from this software without specific prior written
+      permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
 # This code was made to take in 3 files from PEAKs:
 # feature, peptides, protein-peptides
 # TODO: Drop duplicates after sorting for best results
@@ -38,7 +72,6 @@ class Peaks85(BaseConverter):
     # These are the most likely things to change in different versions
     correct_header_names = {
         'peptide': 'Sequence',
-        # change the name of rt_mean to P.R.T (sec)
         'rt_mean': 'Precursor Retention Time (sec)',
         'rt_start': 'rt_start',
         'rt_end': 'rt_end',
@@ -61,7 +94,6 @@ class Peaks85(BaseConverter):
         'protein_existence': 'protein_existence',
         'sequence_version': 'sequence_version',
         'cf': 'cf',
-        #'theoretical_mass': 'theoretical_mass',
         'neutromers_to_extract': 'neutromers_to_extract',
         'literature_n': 'literature_n'
     }
@@ -76,7 +108,6 @@ class Peaks85(BaseConverter):
         'rt_width',
         'mz',
         'theoretical_mass',
-        #'Peptide Theoretical Mass',
         'z',
         'ptm',
         'quality',
@@ -91,7 +122,6 @@ class Peaks85(BaseConverter):
         'protein_existence',
         'sequence_version',
         'cf',
-        #'theoretical_mass',
         'neutromers_to_extract',
         'literature_n'
     ]
@@ -128,7 +158,6 @@ class Peaks85(BaseConverter):
         )
 
         funcs = [
-            # Peaks85._name_filter,
             Peaks85._initial_filters,
             Peaks85._interpret_aa_sequences,
             Peaks85._set_n_peaks,
--- deuteconvert/peaksXpro.py
+++ deuteconvert/peaksXpro.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2016-2020 Bradley Naylor, Michael Porter, Kyle Cutler, Chad Quilling, J.C. Price, and Brigham Young University
+All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are permitted provided
+that the following conditions are met:
+    * Redistributions of source code must retain the
+      above copyright notice, this list of conditions
+      and the following disclaimer.
+    * Redistributions in binary form must reproduce
+      the above copyright notice, this list of conditions
+      and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the author nor the names of any contributors
+      may be used to endorse or promote products derived
+      from this software without specific prior written
+      permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 # This code was made to take in 3 files from PEAKs:
 # feature, peptides, protein-peptides
 # TODO: Drop duplicates after sorting for best results
@@ -128,7 +161,6 @@ class PeaksXpro(BaseConverter):
         )
 
         funcs = [
-            # Peaks85._name_filter,
             PeaksXpro._initial_filters,
             PeaksXpro._interpret_aa_sequences,
             PeaksXpro._set_n_peaks,
