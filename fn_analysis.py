162adc777080d33dd41c6c22ae387f8aa9204759
Ben Driggs <bendriggs99@gmail.com>
2024-10-11 14:01:45 -0600

    Updated todo list and improved fn_analysis

--- fn_analysis.py
+++ fn_analysis.py
@@ -5,20 +5,32 @@ import os
 
 
 def main():
+    # gather files
     root = tk.Tk()
     files = fd.askopenfilenames(parent=root, title='Choose file(s)')
 
     for file in files:
         df = pd.read_csv(file, sep='\t')
-        df = df.loc[df['enrichment'] == 0.05]
+
+        # determine if day zero rows had a set enrichment
+        day_zero = df.loc[df['time'] < 0.1]
+        if day_zero['enrichment'].iloc[0] == 0.05:
+            df = df.loc[df['enrichment'] == 0.05]
+        else:
+            df = df.loc[df['enrichment'] < 0.01]
+
+        # convert cfn to float and ignore cells with text
         df = df[pd.to_numeric(df['cfn'], errors='coerce').notnull()]
         df['cfn'] = df['cfn'].astype(float)
+
+        # calculate average std_dev and fraction new
         avg_std = df['frac_new_combined_std_dev'].mean()
         cfn = df['cfn'].mean()
-        basename = os.path.basename(file)
 
-        print(f"{basename} combined average standard deviation: {avg_std}")
-        print(f"{basename} combined average fraction new: {cfn}")
+        # print results
+        basename = os.path.basename(file)
+        print(f"{basename} combined average standard deviation: {round(avg_std, 4)}")
+        print(f"{basename} combined average fraction new: {round(cfn, 4)}\n")
 
 
 if __name__ == "__main__":
